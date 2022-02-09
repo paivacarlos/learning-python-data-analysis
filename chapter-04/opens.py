61b82196939a11c24b2b8d96559cfe32c5b7c4d3
Carlos Paiva <paivakk22@gmail.com>
2022-02-09 18:48:19 -0300

    Trabalhando com o open() parte 2

--- chapter-04/opens.py
+++ chapter-04/opens.py
@@ -1,5 +1,14 @@
 #Definindo o arquivo que será lido.
-myFile = open("C:/Users/carlo/PycharmProjects/fundamentos/chapter-04/files/file-01.txt", "r")
+myFile = open("files/file-01.txt", "r")
 
 #Lendo o arquivo
 print(myFile.read())
+
+#Contar o numero de caracteres
+print(myFile.tell())
+
+#Retornar para o inicio do arquivo
+print(myFile.seek(0,0))
+
+#Ler os primieros 10 caracteres
+print(myFile.read(10))
