0ede6a4e2fc9fddc55444cfa56b48f6c12a0787b
Carlos Paiva <paivakk22@gmail.com>
2022-03-19 11:06:50 -0300

    feat: POO in Python lesson 02

---
+++ chapter-05/lesson/exe-02.py
@@ -0,0 +1,21 @@
+# Exercício 2 - Crie uma classe chamada Pessoa() com os atributos:
+# nome, cidade, telefone e e-mail. Use pelo menos 2
+# métodos especiais na sua classe.
+# Crie um objeto da sua classe e faça uma chamada a pelo menos um dos seus métodos
+# especiais.
+
+class People():
+    def __init__(self, name, city, phone, email):
+        self.name = name
+        self.city = city
+        self.phone = phone
+        self.email = email
+        print("People created!")
+
+    def __str__(self):
+        return f"Nome: {self.name}, Cidade: {self.city}, Fone: {self.phone}, Email: {self.email}"
+
+people01 = People("Carlos Paiva", "São Lepoldo", "51-997064415", "paivakk22@gmail.com")
+print(people01)
+print(getattr(people01, "name"))
+print(hash(people01))
